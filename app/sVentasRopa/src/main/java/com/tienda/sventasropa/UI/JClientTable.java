ec9b5477696e79ad3f8e775ad6134eadae685bf8
Mefiss1 <christopher.lazaro@udea.edu.co>
2026-04-09 23:20:21 -0500

    Update JClientTable.java

--- app/sVentasRopa/src/main/java/com/tienda/sventasropa/UI/JClientTable.java
+++ app/sVentasRopa/src/main/java/com/tienda/sventasropa/UI/JClientTable.java
@@ -10,8 +10,8 @@ import java.util.List;
 import javax.swing.table.DefaultTableModel;
 
 /**
- * Internal frame that displays clients in a table.
- * This frame can be added to a JDesktopPane for MDI-style display.
+ * JFrame que sera usado cuando el usuario quiera ver los clientes registrados.
+ * Se llamará atravez de un menu MDI
  * @author Christopher
  */
 public class JClientTable extends javax.swing.JInternalFrame {
@@ -71,7 +71,7 @@ public class JClientTable extends javax.swing.JInternalFrame {
     private void initComponents() {
 
         Background = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
+        jPanelShowClient = new javax.swing.JScrollPane();
         clientTable = new javax.swing.JTable();
 
         setClosable(true);
@@ -86,17 +86,17 @@ public class JClientTable extends javax.swing.JInternalFrame {
             new Object[][]{},
             new String[]{"ID", "Nombre", "Correo", "Teléfono"}
         ));
-        jScrollPane1.setViewportView(clientTable);
+        jPanelShowClient.setViewportView(clientTable);
 
         javax.swing.GroupLayout BackgroundLayout = new javax.swing.GroupLayout(Background);
         Background.setLayout(BackgroundLayout);
         BackgroundLayout.setHorizontalGroup(
             BackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 394, Short.MAX_VALUE)
+            .addComponent(jPanelShowClient, javax.swing.GroupLayout.DEFAULT_SIZE, 394, Short.MAX_VALUE)
         );
         BackgroundLayout.setVerticalGroup(
             BackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 268, Short.MAX_VALUE)
+            .addComponent(jPanelShowClient, javax.swing.GroupLayout.DEFAULT_SIZE, 268, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -119,6 +119,6 @@ public class JClientTable extends javax.swing.JInternalFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Background;
     private javax.swing.JTable clientTable;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jPanelShowClient;
     // End of variables declaration//GEN-END:variables
 }
