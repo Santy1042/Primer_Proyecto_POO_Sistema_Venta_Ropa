2915c1a7c23e4f4f9710379bc1f76fa8ce9d87cf
Santiago <santipenguin777@gmail.com>
2026-04-09 21:40:06 -0500

    Modulo De Productos Aprobado
    Se aprueba el modulo de productos.

--- app/sVentasRopa/src/main/java/com/tienda/sventasropa/interfaces/IClientRepository.java
+++ app/sVentasRopa/src/main/java/com/tienda/sventasropa/interfaces/IClientRepository.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package com.tienda.sventasropa.repository;
+package com.tienda.sventasropa.interfaces;
 
 import com.tienda.sventasropa.model.Client;
 import java.util.List;
---
+++ app/sVentasRopa/src/main/java/com/tienda/sventasropa/interfaces/IProductRepository.java
@@ -0,0 +1,17 @@
+package com.tienda.sventasropa.interfaces;
+
+import com.tienda.sventasropa.model.Product;
+import java.util.List;
+
+/**
+ *
+ * @author Santy
+ */
+public interface IProductRepository {
+    boolean addProduct (Product producto);
+    boolean deleteProduct (int productId);
+    boolean updateProduct (int productId, Product updatedProduct);
+    Product findProductById (int productId);
+    List<Product> getAllProducts();
+
+}
