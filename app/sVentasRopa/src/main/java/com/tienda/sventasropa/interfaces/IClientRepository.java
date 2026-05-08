461a0324cdcd342fffc91f9cb4631b75089c5ba8
brillos-21 <brigittes0105@gmail.com>
2026-05-07 20:02:26 -0500

    Se mejoran validaciones y operaciones del módulo de clientes

--- app/sVentasRopa/src/main/java/com/tienda/sventasropa/interfaces/IClientRepository.java
+++ app/sVentasRopa/src/main/java/com/tienda/sventasropa/interfaces/IClientRepository.java
@@ -1,20 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.tienda.sventasropa.interfaces;
 
 import java.util.List;
+
 import com.tienda.sventasropa.model.Client;
 
-/**
- * Interfaz para operaciones de repositorio de clientes
- * Define los métodos CRUD para gestionar clientes
- */
 public interface IClientRepository {
+
     boolean save(Client cliente);
-    void editClient(int id, Client datosNuevos);
-    void deleteClient(int id);
+
+    boolean editClient(int id, Client datosNuevos);
+
+    boolean deleteClient(int id);
+
     Client findClientById(int id);
+
     List<Client> getAllClients();
 }
